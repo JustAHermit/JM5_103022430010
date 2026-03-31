3ba98eb2d08779f23aa14248d130c661ac8ba0a3
JustAHermit <arsall1202@gmail.com>
2026-03-31 07:36:11 +0700

    menambahkan class PemrosesData dengan method  DapatkanNilaiTerbesar

---
+++ Modul5_103022430010/Modul5_103022430010/Program.cs
@@ -0,0 +1,31 @@
+﻿public class PemrosesData{
+    public T DapatkanNilaiTerbesar<T> (T nilai1, T nilai2, T nilai3)
+    {
+        dynamic nil1 = (dynamic)nilai1;
+        dynamic nil2 = (dynamic)nilai2;
+        dynamic nil3 = (dynamic)nilai3;
+        dynamic nilB = nil1;
+
+        if (nil2 > nilB)
+        {
+            nilB = nil2;
+        }
+
+        if (nil3 > nilB)
+        {
+            nilB = nil3;
+        }
+        return nilB;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PemrosesData data = new PemrosesData();
+        long nilB = data.DapatkanNilaiTerbesar(10, 30, 22);
+        Console.WriteLine("Nilai Terbesar Adalah : " + nilB);
+    }
+}
+
